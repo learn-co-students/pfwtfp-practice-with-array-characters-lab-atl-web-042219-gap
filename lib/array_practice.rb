579ed6fae7ddc4f162481ee0b30861f39372f227
github username <github email address>
2019-05-16 23:19:32 -0400

    Done.

--- lib/array_practice.rb
+++ lib/array_practice.rb
@@ -1,1 +1,37 @@
+require 'pry'
 #Write your code here
+def sentence_to_array(sentence)
+    sentence.split(" ")
+end
+
+def clean_up_elements(sentence)
+    cleaned_sentence =  sentence.reject{|wrd| wrd.nil? || wrd == " " || wrd.empty?}
+    sentence = cleaned_sentence.map{|word| word.strip}
+end
+
+def array_to_sentence(sentence)
+    sentence.join(" ")
+end
+
+def capitalize_all_elements(sentence)
+   sentence.map do |word| 
+            if word.length > 1
+                word.capitalize
+            else 
+                word.upcase
+            end
+        end
+end
+
+def substitute_strings
+
+end
+
+def title_format(sentence)
+ sentence.map do |wrd|
+       wrd.titleize
+        end
+end
+
+
+
